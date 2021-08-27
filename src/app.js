05806e0a012c2b08299721c52a6372da02b62e86
mamnoonsami <mamnoon909@gmail.com>
2021-08-26 23:24:04 -0300

    setup app for Heroku

--- src/app.js
+++ src/app.js
@@ -6,6 +6,8 @@ const forecast = require('./utils/forecast')
 
 const app = express()
 
+const port = process.env.PORT || 3000
+
 //Define paths for express config
 const publicDirectory = path.join(__dirname, '../public') // goes on dir up and goes to public folder
 const viewsPath = path.join(__dirname, '../templates/views')
@@ -106,6 +108,6 @@ app.get('*', (req, res) => { // if any invalid link is typed like localhost:3000
     })
 })
 
-app.listen(3000, () => {
-        console.log('Server is online on port 3000.')
+app.listen(port, () => {
+        console.log('Server is online on port ' + port)
     }) // starts the server, 3000 is common developer port, for http its 80, 3000 for local developement port
